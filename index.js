de8177fd533d7296ec4ee1974bcf2e7af088e39d
OLI11GH <86797838+oli11gh@users.noreply.github.com>
2024-04-23 19:51:23 +0200

    password version

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const app = express();
 const os = require('os');
 const si = require('systeminformation');
@@ -12,22 +15,77 @@ app.use(
     express.static(path.join(__dirname, 'public'))
   );
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+      secret: '8h5er6h7886b7nr4v57n865er897Y78976BTIDTYBUGGFDVJHBGVYB67E6B7IE6B7VTRIT6B757I6TVRIT7B6DETV6B7IRD',
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        if (username === 'admin' && password === 'admin') {
+            return done(null, { id: 1, username: 'admin' });
+        } else {
+            return done(null, false);
+        }
+    })
+);
 
-app.get('/', async (req, res) => {
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    const user = { id: 1, username: 'admin' };
+    done(null, user);
+});
+
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+};
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
+);
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error:', err);
+        } else {
+            res.redirect('/login');
+        }
+    });
+});
+
+app.get('/', isAuthenticated, async (req, res) => {
     try {
         res.render('index');
     } catch (err) {
         console.error(err);
-        res.status(500).send('Internal Error');
+        res.status(500).send('Internal error');
     }
 });
-app.get('/stats', async (req, res) => {
+app.get('/stats', isAuthenticated, async (req, res) => {
     try {
         const stats = await getServerStats();
         res.json(stats);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: 'Internal Error' });
+        res.status(500).json({ error: 'Internal error' });
     }
 });
 async function getServerStats() {
@@ -55,13 +113,13 @@ async function getServerStats() {
             cpuUsage: cpuUsage.toFixed(2),
             ramUsage: `${(ramUsedGB).toFixed(2)}GB / ${(ramTotal / (1024 * 1024 * 1024)).toFixed(2)}GB - ${ramUsagePercent.toFixed(2)}%`,
             diskUsage: `${(diskUsed / (1024 * 1024 * 1024)).toFixed(2)}GB / ${(diskTotal / (1024 * 1024 * 1024)).toFixed(2)}GB - ${diskUsagePercent.toFixed(2)}%`,
-            uptime: `${uptimeDays} days ${uptimeHours} hours ${uptimeMinutes} minutes ${uptimeSeconds} secound`,
+            uptime: `${uptimeDays} days ${uptimeHours} hours ${uptimeMinutes} minutes ${uptimeSeconds} seconds`,
             ramUsedGB: ramUsedGB.toFixed(2)
         };
 
         return stats;
     } catch (error) {
-        console.error("Error:", error);
+        console.error("Stats error:", error);
         return {
             cpuUsage: 'N/A',
             ramUsage: 'N/A',
